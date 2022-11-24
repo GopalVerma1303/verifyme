23b56fa559713298321fd6331f60a9489c04ef59
ug21103079 <96766463+ug21103079@users.noreply.github.com>
2022-11-25 02:47:36 +0530

    Login

--- components/AdminContainer.js
+++ components/AdminContainer.js
@@ -15,30 +15,20 @@ const AdminContainer = () => {
                 </p>
             </div>
             <div>
-                <div className="flex p-4 space-x-3">
+                <div className="flex p-4 space-x-12 ml-12">
+                    <Card title="TOTAL" balance={409.0790} icon={0} />
+                    <Card title="AVAILABLE" balance={300.0790} icon={0 } />
+                    <Card title="AVAILABLE" balance={300.0790} icon={0} />
+                </div>
+                <div className="flex p-4 space-x-12 ml-12 mt-4">
                     <Card title="TOTAL" balance={409.0790} icon={0} />
                     <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
+                    <Card title="AVAILABLE" balance={300.0790} icon={1} />
                 </div>
-                <div className="flex p-4 space-x-3">
+                <div className="flex p-4 space-x-12 ml-12 mt-4">
                     <Card title="TOTAL" balance={409.0790} icon={0} />
                     <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
-                </div>
-                <div className="flex p-4 space-x-3">
-                    <Card title="TOTAL" balance={409.0790} icon={0} />
-                    <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
-                </div>
-                <div className="flex p-4 space-x-3">
-                    <Card title="TOTAL" balance={409.0790} icon={0} />
-                    <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
-                </div>
+                </div>  
             </div>
             {/* <div className="flex  ml-3 mt-6 space-x-6  mr-4">
             </div> */}
