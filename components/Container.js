4788938225bf8a884b6dec11f2cb896145f270d9
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-11-25 03:28:43 +0530

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Card from './Card'
-import Middle from './Middle'
-import RightBar from './RightBar'
+import Card from '../components/Card'
+// import Middle from './Middle'
+// import RightBar from './RightBar'
 
-const Container = () => {
+const Container = (props) => {
     return (
         <div className=" bg-gradient-to-r   " >
             <div className="  px-8 py-1 ">
@@ -15,29 +15,20 @@ const Container = () => {
                 </p>
             </div>
             <div>
-                <div className="flex p-4 space-x-3">
-                    <Card title="TOTAL" balance={409.0790} icon={0} />
-                    <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
+                <div className="flex p-4 mt-4 space-x-12 ml-40">
+                    <Card title="TOTAL" balance={409.0790} icon={0} type="unchecked" />
+                    <Card title="AVAILABLE" balance={300.0790} icon={1} type="unchecked" />
+                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} type="unchecked" />
                 </div>
-                <div className="flex p-4 space-x-3">
-                    <Card title="TOTAL" balance={409.0790} icon={0} />
-                    <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
+                <div className="flex p-4 space-x-12 ml-40">
+                    <Card title="TOTAL" balance={409.0790} icon={0} type="unchecked" />
+                    <Card title="AVAILABLE" balance={300.0790} icon={1} type="unchecked" />
+                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} type="unchecked" />
                 </div>
-                <div className="flex p-4 space-x-3">
-                    <Card title="TOTAL" balance={409.0790} icon={0} />
-                    <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
-                </div>
-                <div className="flex p-4 space-x-3">
-                    <Card title="TOTAL" balance={409.0790} icon={0} />
-                    <Card title="AVAILABLE" balance={300.0790} icon={1} />
-                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} />
-                    <Card title="DELEGATED" balance={339.0790} icon={3} />
+                <div className="flex p-4 space-x-12 ml-40">
+                    <Card title="TOTAL" balance={409.0790} icon={0} type="unchecked" />
+                    <Card title="AVAILABLE" balance={300.0790} icon={1} type="unchecked" />
+                    <Card title="CLAIMABLE REWARD" balance={100.0790} icon={2} type="unchecked" />
                 </div>
             </div>
             {/* <div className="flex  ml-3 mt-6 space-x-6  mr-4">
