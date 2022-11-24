02b990d575c98b429d1764ecf13027c188752b86
Gopal Verma <67120469+GopalVerma1303@users.noreply.github.com>
2022-11-24 22:47:52 +0530

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  initialSession: Session,
-}>) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
-
-  return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <div className=' h-screen overflow-y-scroll bg-slate-200'>
-        <Component {...pageProps} />
-      </div>
-    </SessionContextProvider>
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+
 export default MyApp
